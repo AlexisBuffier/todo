18c005a0d4bd12d82bf4b8fd4e48fc8c3294df85
Alexis <alexis.buffier0100@orange.fr>
2023-05-30 11:51:03 +0200

    donnee

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Todo;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class AppFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager): void
+    {
+
+        $faker = Faker\Factory::create('fr_FR');
+           
+           $todo = Array();
+           for ($i = 0; $i < 50; $i++) {
+               $todo[$i] = new Todo();
+               $todo[$i]->setName($faker->name);
+               $todo[$i]->setDescription($faker->sentence());
+
+               $manager->persist($todo[$i]);
+           }
+
+        $manager->flush();
+    }
+}
